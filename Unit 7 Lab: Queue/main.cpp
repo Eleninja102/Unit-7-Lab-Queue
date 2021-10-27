ed9ae79aa7d15655cff5c07c0cb0200a33fd1eba
Coleton Watt <electricninja102@gmail.com>
2021-10-27 09:30:10 -0600

    Initial Commit

---
+++ Unit 7 Lab: Queue/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit 7 Lab: Queue
+//
+//  Created by Coleton Watt on 10/27/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
